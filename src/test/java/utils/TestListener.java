1fb84b19c5fe02b3cc87b6224388ff10add1cb97
Pramod Salunkhe <psalunkhe72@gmail.com>
2025-09-06 15:17:01 +0530

    Add Maven + TestNG + ExtentReports setup with TestListener

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -14,6 +14,8 @@ import java.nio.file.Files;
 
 public class TestListener implements ITestListener {
 
+
+
     @Override
     public void onTestStart(ITestResult result) {
         ExtentManager.createTest(result.getMethod().getMethodName());
